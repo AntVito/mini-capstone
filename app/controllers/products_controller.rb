41e016e9e202091e82ed70b34920bbc62cc78d56
Anthony <anthonyheadcase@yahoo.com>
2017-05-03 14:38:24 -0700

    commit after undrstandingsetting attributes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,10 @@
 class ProductsController < ApplicationController
   def index_method
-
+    @products = Product.all
     render "index.html.erb"
   end
   def all_info_method
-
+    @products = Product.all
     render "index.html.erb"
   end
 end
